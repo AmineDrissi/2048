1b0fab13efed43f52dd472e7e684a6b18b3cc632
AmineDrissi <aminedrissi514@gmail.com>
2023-08-08 10:33:58 +0100

    finalWork

--- main.js
+++ main.js
@@ -5,16 +5,11 @@ for(var i=1;i<=4;i++){
     }
 }
 $("#startButton").on("click",startGame)
-
+$("#resetButton").on("click",reset)
+var score=0
+$("h2").text("Score:"+score)
 function startGame(){
-    //this restarts the board to empty boxes
-    for(var i=1;i<=4;i++){
-        for(var j=1;j<=4;j++){
-            $("#"+i+"-"+j).removeClass()
-            $("#"+i+"-"+j).text("")
-            $("#"+i+"-"+j).addClass("tile x")
-        }
-    }
+    $('#startButton').prop('disabled', true)
     //this generates 2 random boxes with values 2
     var randomPosition1=Math.floor((Math.random()*4)+1)+"-"+Math.floor((Math.random()*4)+1)
     var randomPosition2=Math.floor((Math.random()*4)+1)+"-"+Math.floor((Math.random()*4)+1)
@@ -32,17 +27,44 @@ function startGame(){
         var direction=e.key
         switch(direction){
             case "ArrowUp":up();
+                            checkPlay();
+                            spawRandom2();
                             break;
             case "ArrowDown":down();
+                            checkPlay();
+                            spawRandom2();
                             break;
             case "ArrowLeft":left();
+                            checkPlay();
+                            spawRandom2();
                             break;
             case "ArrowRight":right();
+                            checkPlay();
+                            spawRandom2();
                             break;
         }
     })
 }
+function reset(){
+    location.reload(true)
+    //this restarts the board to empty boxes
+    for(var i=1;i<=4;i++){
+        for(var j=1;j<=4;j++){
+            $("#"+i+"-"+j).remove()
+        }
+    }
+    for(var i=1;i<=4;i++){
+        for(var j=1;j<=4;j++){
+            $(".board").append("<div class='tile x' id='"+ i+"-"+j+"'></div>")
+        }
+    }
+}
+var flexibilityUp=true
+var flexibilityDown=true
+var flexibilityLeft=true
+var flexibilityRight=true
 function up(){
+    var oldScore=score
     for(var k=0;k<4;k++){
         for(var i=1;i<=4;i++){
             for(var j=1;j<=4;j++){
@@ -61,6 +83,8 @@ function up(){
                     var classNumberValue=$("#"+identificator).attr("class")
                     var numberValue=parseInt($("#"+identificator).text())
                     numberValue=numberValue*2
+                    score+=numberValue
+                    $("h2").text("Score:"+score)
                     classNumberValue="tile x"+numberValue
                     $("#"+(i-1)+"-"+j).removeClass()
                     $("#"+(i-1)+"-"+j).addClass(classNumberValue)
@@ -72,14 +96,12 @@ function up(){
             }
         }
     }
-    var emptyBoxes=getEmptyBoxes()
-    var randomIndex=Math.floor(Math.random()*(emptyBoxes.length))
-    var randomPosition=emptyBoxes[randomIndex]
-    $("#"+randomPosition).removeClass()
-    $("#"+randomPosition).addClass("tile x2")
-    $("#"+randomPosition).text("2")
+    if(oldScore===score){
+        flexibilityUp=false
+    }
 }
 function down(){
+    var oldScore=score
     for(var k=0;k<4;k++){
         for(var i=1;i<=4;i++){
             for(var j=1;j<=4;j++){
@@ -98,6 +120,8 @@ function down(){
                     var classNumberValue=$("#"+identificator).attr("class")
                     var numberValue=parseInt($("#"+identificator).text())
                     numberValue=numberValue*2
+                    score+=numberValue
+                    $("h2").text("Score:"+score)
                     classNumberValue="tile x"+numberValue
                     $("#"+(i+1)+"-"+j).removeClass()
                     $("#"+(i+1)+"-"+j).addClass(classNumberValue)
@@ -109,14 +133,12 @@ function down(){
             }
         }
     }
-    var emptyBoxes=getEmptyBoxes()
-    var randomIndex=Math.floor(Math.random()*(emptyBoxes.length))
-    var randomPosition=emptyBoxes[randomIndex]
-    $("#"+randomPosition).removeClass()
-    $("#"+randomPosition).addClass("tile x2")
-    $("#"+randomPosition).text("2")
+    if(oldScore===score){
+        flexibilityDown=false
+    }
 }
 function left(){
+    var oldScore=score
     for(var k=0;k<4;k++){
         for(var i=1;i<=4;i++){
             for(var j=1;j<=4;j++){
@@ -135,6 +157,8 @@ function left(){
                     var classNumberValue=$("#"+identificator).attr("class")
                     var numberValue=parseInt($("#"+identificator).text())
                     numberValue=numberValue*2
+                    score+=numberValue
+                    $("h2").text("Score:"+score)
                     classNumberValue="tile x"+numberValue
                     $("#"+i+"-"+(j-1)).removeClass()
                     $("#"+i+"-"+(j-1)).addClass(classNumberValue)
@@ -146,14 +170,12 @@ function left(){
             }
         }
     }
-    var emptyBoxes=getEmptyBoxes()
-    var randomIndex=Math.floor(Math.random()*(emptyBoxes.length))
-    var randomPosition=emptyBoxes[randomIndex]
-    $("#"+randomPosition).removeClass()
-    $("#"+randomPosition).addClass("tile x2")
-    $("#"+randomPosition).text("2")
+    if(oldScore===score){
+        flexibilityLeft=false
+    }
 }
 function right(){
+    var oldScore=score
     for(var k=0;k<4;k++){
         for(var i=1;i<=4;i++){
             for(var j=1;j<=4;j++){
@@ -172,6 +194,8 @@ function right(){
                     var classNumberValue=$("#"+identificator).attr("class")
                     var numberValue=parseInt($("#"+identificator).text())
                     numberValue=numberValue*2
+                    score+=numberValue
+                    $("h2").text("Score:"+score)
                     classNumberValue="tile x"+numberValue
                     $("#"+i+"-"+(j+1)).removeClass()
                     $("#"+i+"-"+(j+1)).addClass(classNumberValue)
@@ -183,12 +207,9 @@ function right(){
             }
         }
     }
-    var emptyBoxes=getEmptyBoxes()
-    var randomIndex=Math.floor(Math.random()*(emptyBoxes.length))
-    var randomPosition=emptyBoxes[randomIndex]
-    $("#"+randomPosition).removeClass()
-    $("#"+randomPosition).addClass("tile x2")
-    $("#"+randomPosition).text("2")
+    if(oldScore===score){
+        flexibilityRight=false
+    }
 }
 function getEmptyBoxes(){
     var result=[]
@@ -201,4 +222,23 @@ function getEmptyBoxes(){
         }
     }
     return result
+}
+function spawRandom2(){
+    var emptyBoxes=getEmptyBoxes()
+    var randomIndex=Math.floor(Math.random()*(emptyBoxes.length))
+    var randomPosition=emptyBoxes[randomIndex]
+    $("#"+randomPosition).removeClass()
+    $("#"+randomPosition).addClass("tile x2")
+    $("#"+randomPosition).text("2")
+}
+function checkPlay(){
+    if(flexibilityDown===false && flexibilityLeft===false && flexibilityRight===false && flexibilityUp===false){
+        console.log("you lost");
+    }
+    else{
+        flexibilityDown=true
+        flexibilityUp=true
+        flexibilityLeft=true
+        flexibilityRight=true
+    }
 }
